a98afb7ae4ed3eaa136dbdff50082f97c8e0070b
Mast3rM0ds <applab766@gmail.com>
2025-11-10 19:41:43 +0100

    Remove obsolete Window and main files

--- src/WinView/Window.d
+++
@@ -1,5 +0,0 @@
-src/WinView/Window.o: src/WinView/Window.cpp \
- src/WinView/../Headers/Window/Window.h \
- src/WinView/../Headers/Window/../General/GlobalHeaders.h
-src/WinView/../Headers/Window/Window.h:
-src/WinView/../Headers/Window/../General/GlobalHeaders.h:
--- src/main.d
+++
@@ -1,4 +0,0 @@
-src/main.o: src/main.cpp src/Headers/Window/Window.h \
- src/Headers/Window/../General/GlobalHeaders.h
-src/Headers/Window/Window.h:
-src/Headers/Window/../General/GlobalHeaders.h:
