1e4eb32c69209898807c7a43bb1d95c3c27409f5
Anemunt <69436164+darkresident55@users.noreply.github.com>
2025-11-29 21:07:37 -0500

    some shader stuff

--- Resources/Shaders/frag.glsl
+++ Resources/Shaders/frag.glsl
@@ -2,11 +2,39 @@
 out vec4 FragColor;
 
 in vec2 TexCoord;
+in vec3 FragPos;
+in vec3 Normal;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
+// Lighting uniforms
+uniform vec3 lightPos;      // Position of the light in world space
+uniform vec3 lightColor = vec3(1.0, 1.0, 1.0);
+uniform vec3 objectColor = vec3(1.0, 1.0, 1.0);  // tint (optional)
+
+uniform float ambientStrength = 0.1;
+uniform float mixAmount = 0.2;  // how much of texture2 to blend
+
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    // --- Texture sampling & mixing ---
+    vec4 tex1 = texture(texture1, TexCoord);
+    vec4 tex2 = texture(texture2, TexCoord);
+    vec4 texColor = mix(tex1, tex2, mixAmount);
+
+    // --- Ambient ---
+    vec3 ambient = ambientStrength * lightColor;
+
+    // --- Diffuse ---
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // --- Combine lighting ---
+    vec3 lighting = ambient + diffuse;
+    vec3 result = lighting * objectColor * texColor.rgb;
+
+    FragColor = vec4(result, texColor.a);
 }
--- Resources/Shaders/vert.glsl
+++ Resources/Shaders/vert.glsl
@@ -1,8 +1,12 @@
 #version 330 core
+
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout (location = 1) in vec3 aNormal;   // NEW: normal attribute
+layout (location = 2) in vec2 aTexCoord;
 
 out vec2 TexCoord;
+out vec3 FragPos;      // Position in world space
+out vec3 Normal;       // Normal in world space
 
 uniform mat4 model;
 uniform mat4 view;
@@ -10,6 +14,15 @@ uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    // World-space position
+    vec4 worldPos = model * vec4(aPos, 1.0);
+    FragPos = worldPos.xyz;
+
+    // Transform normal to world space (non-uniform scale safe)
+    Normal = normalize(mat3(transpose(inverse(model))) * aNormal);
+
+    // Clip-space position
+    gl_Position = projection * view * worldPos;
+
     TexCoord = aTexCoord;
 }
